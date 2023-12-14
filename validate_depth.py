26dc2f5575c08761ff63cca7d734c647109c2adb
abemelech <abemelech.mesfin@gmail.com>
2023-12-13 23:12:11 -0600

    feat: add validation step

---
+++ validate_depth.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+
+
+class ValidateDepth:
+    def __init__(self, generated_depth: pd.DataFrame, true_depth_path) -> None:
+        self.generated_depth = generated_depth
+        self.true_depth = pd.read_csv(true_depth_path)
+
+    def validate(self):
+        assert (
+            self.generated_depth.shape == self.true_depth.values
+        ), "Depth maps are not the same size"
+
+        diff = np.abs(self.generated_depth.values - self.true_depth.values)
+
+        # Calculate Mean Absolute Error (MAE)
+        mae = np.mean(diff)
+
+        # Calculate Root Mean Squared Error
+        rmse = np.sqrt(np.mean(diff*2))
+
+        print(f"MAE: {mae}")
+        print(f"RMSE: {rmse}")
